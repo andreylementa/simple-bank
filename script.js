920442394bba4c83497b33a763c0f3cecea91be7
Andrey Lementa <andrejkudrasov79070@gmail.com>
2023-06-04 10:53:37 +0300

    Виды событий и обработчиков

--- script.js
+++ script.js
@@ -60,7 +60,9 @@ document
     message.remove();
     // message.parentElement.removeChild(message);
   });
-console.log(getComputedStyle(overlay));
+//console.log(getComputedStyle(overlay));
+
+// Плавное прокручивание
 
 const section1 = document.querySelector('#section--1');
 const btnScroll = document.querySelector('.btn--scroll-to');
@@ -68,3 +70,23 @@ const btnScroll = document.querySelector('.btn--scroll-to');
 btnScroll.addEventListener('click', function (e) {
   section1.scrollIntoView({ behavior: 'smooth' });
 });
+
+/////////////////////////////////////////////////
+// Виды Событий И Обработчиков Событий
+
+const h1 = document.querySelector('h1');
+// const alertMouseEnterH1 = function (e) {
+//   alert('addEventListener: You are now at the h1 element');
+//   h1.removeEventListener('mouseenter', alertMouseEnterH1);
+// };
+
+const alertMouseEnterH1 = function (e) {
+  alert('addEventListener: You are now at the h1 element');
+};
+h1.addEventListener('mouseenter', alertMouseEnterH1);
+
+setTimeout(() => h1.removeEventListener('mouseenter', alertMouseEnterH1), 3000);
+
+// h1.onclick = function (e) {
+//   alert('onclick: You have clicked the h1 element');
+// };
