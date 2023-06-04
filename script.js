4330e8849e6e898ac7305fac47ac114e7ddeb4ba
Andrey Lementa <andrejkudrasov79070@gmail.com>
2023-06-04 12:33:00 +0300

    Перемещение по DOM

--- script.js
+++ script.js
@@ -162,3 +162,31 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
 //   console.log('Body', e.target, e.currentTarget);
 //   console.log(this === e.currentTarget);
 // });
+
+//////////////////////////////////////////////
+// DOM traversing (Перемещение по DOM)
+
+// const h1 = document.querySelector('h1');
+
+// // Перемещение вниз (к потомкам)
+// console.log(h1.querySelectorAll('.highlight'));
+// console.log(h1.childNodes);
+// console.log(h1.children);
+// console.log(h1.firstElementChild);
+// h1.firstElementChild.style.color = 'yellow';
+// console.log(h1.lastElementChild);
+
+// // Перемещение вверх (к родителям)
+// console.log(h1.parentNode);
+// console.log(h1.parentElement);
+
+// const h2 = document.querySelector('h2');
+// console.log(h2);
+// // h2.closest('.section').style.backgroundColor = 'blue';
+// // h2.closest('h2').style.backgroundColor = 'green';
+
+// // Перемещение в стороны
+// console.log(h2.previousElementSibling);
+// console.log(h2.nextElementSibling);
+
+// console.log(h1.parentElement.children);
